9f56a57a85e9e6f726a3bc9d43619ac5cdf96158
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-08-29 14:55:30 +0300

    Initial Commit

---
+++ SwoopyuiTemplate-iOS/SwoopyuiTemplate_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwoopyuiTemplate_iOSApp.swift
+//  SwoopyuiTemplate-iOS
+//
+//  Created by Yousif Aladwani on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwoopyuiTemplate_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
